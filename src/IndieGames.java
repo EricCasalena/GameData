541c02f135641ed282c497dcaaf3a206634643e3
Sean081 <SeanHogan081@gmail.com>
2022-05-06 13:45:52 -0400

    Added Release Year to the output

--- src/IndieGames.java
+++ src/IndieGames.java
@@ -6,7 +6,7 @@ public class IndieGames {
     String publisher;
 
     void describe() {
-        System.out.println("This game is called " + title + " The game sold " + sales + " It is developed by " + developer + " it is published by " + publisher);
+        System.out.println("This game is called " + title + " The game released in " + release + " The game sold " + sales + " It is developed by " + developer + " it is published by " + publisher);
     }
 }
 
